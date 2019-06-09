cfc6028895fba73c77fbbf82f3b0434e4989b20a
Abhay <abhay.sehgal2012@gmail.com>
2019-06-09 14:20:09 +0530

    Query Retry Support Added

--- lib/index.ts
+++ lib/index.ts
@@ -2,8 +2,11 @@
  * This is a Wrapper class, which helps to get rid of callbacks of mysql package functions 
  * and provides a way to use them in async await (es7) syntax
  * 
- * Just Import BaseDatabase class and create a custom class which extends BaseDatabase 
- * and pass pool in super (BaseDatabase class constructor)
+ * Just Import BaseDatabase class and create an instance of it by passing pool as 1st parameter
+ * configuration in 2nd parameter
+ * 
+ * It supports retry query execution also, by passing maxRetryCount and retryErrorCodes in 
+ * configuration while creating db instance
  * 
  * To BeginTransaction Just pass transaction true in options while calling getConnection,
  * or if required beginTransaction function can be called separately
@@ -17,6 +20,15 @@ interface IConnectionConfig {
     transaction?: boolean;
 }
 
+/**
+ * @description Database Configuration
+ * @param maxRetryCount Number of Retries in case of errors
+ * @param retryErrorCodes Pass Array of Error Codes like ER_LOCK_DEADLOCK, ERR_LOCK_WAIT_TIMEOUT
+ */
+interface IDBConfig {
+    maxRetryCount?: number;
+    retryErrorCodes?: string[];
+}
 
 class BaseDatabase {
 
@@ -24,8 +36,12 @@ class BaseDatabase {
     private _connection: any = null;
     private _inTransaction: boolean = false;
 
-    constructor(pool: any) {
+    private _maxRetryCount: number = 0;
+    private _retryErrorCodes: string[] = [];
+    constructor(pool: any, dbConfig: IDBConfig = {}) {
         this._pool = pool;
+        this._maxRetryCount = dbConfig.maxRetryCount || 0; 
+        this._retryErrorCodes = dbConfig.retryErrorCodes || [];
     }
 
     /**
@@ -72,27 +88,30 @@ class BaseDatabase {
      * @description To Execute DB Query, In case of Error and Connecton in transaction auto rollback will be called
      * @param query {required} Query String
      * @param queryParams {optional} Query Array
+     * @param retryErrorCodes {optional} Array of Error Codes in which you want to retry for this query 
      */
-    public executeQuery(query: string, queryParams?: any) {
+    public executeQuery(query: string, queryParams?: any, retryErrorCodes?: string[]) {
 
         return new Promise((resolve, reject) => {
             if (this._connection) {
-                const queryString = this._connection.query(query, queryParams, (err: Error, result: any) => {
 
-                    // Incase of Error and Connection in transaction rollback transaction and make inTransaction to Avoid auto commit on connection close
-                    if (err) {
-                        if (this._inTransaction) {
-                            this._inTransaction = false;
-                            this.rollback(() => {
-                                reject(err);
-                            });
-                            return;
-                        }
-                        reject(err);
+                this._executeQuery(query, queryParams, this._retryErrorCodes.concat(retryErrorCodes), 0).then(result => {
+                    
+                    resolve(result);
+
+                }).catch(err => {
+                    // In case of Error in Query and connection is in transaction rollback
+                    if (this._inTransaction) {
+                        this._inTransaction = false;
+                        this.rollback(() => {
+                            reject(err);
+                        });
                         return;
                     }
-                    resolve(result);
+                    reject(err);
+
                 });
+
             } else {
                 reject(new Error("Connection Doesn't Exist"));
             }
@@ -148,6 +167,34 @@ class BaseDatabase {
             this._connection.release();
             this._connection = null;
         }
+    }
+
+    // Executes Query and Retry retryCount times if error code is present in retryErrorCodes
+    private _executeQuery(query: string, queryParams: any, retryErrorCodes: string[], retryCount: number){
+        return new Promise((resolve, reject) => {
+            const queryString = this._connection.query(query, queryParams, async (err: any, result: any) => {
+
+                // Incase of Error and Connection in transaction rollback transaction and make inTransaction to Avoid auto commit on connection close
+                if (err) {
+                    const retryCheck = retryErrorCodes.findIndex(err.code) > -1;
+
+                    if(retryCheck && retryCount <= this._maxRetryCount){
+                        try{
+                            
+                            const retryResult = await this._executeQuery(query, queryParams, retryErrorCodes, retryCount + 1);
+                            resolve(retryResult);
+
+                        }catch(err){
+                            reject(err);
+                        }
+                    }else{
+                        reject(err);
+                    }
+                }else{
+                    resolve(result);
+                }
+            });
+        })
     }
 }
 
